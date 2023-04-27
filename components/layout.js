1bf7617a95ba202d6ffcf2117411cef7f987c675
Preet <patelpreet094@gmail.com>
2023-04-27 01:13:23 -0400

    Added privacy page

--- components/layout.js
+++ components/layout.js
@@ -71,11 +71,11 @@ export default function Layout({ children, home }) {
         )}
       </header> */}
       <main>{children}</main>
-      {!home && (
+      {/* {!home && (
         <div className={styles.backToHome}>
           <Link href="/">← Back to home</Link>
         </div>
-      )}
+      )} */}
       <Footer/>
     </div>
   )
