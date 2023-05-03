752c0d6a53ff2f5b0eefcde19f5fa73cf858d816
Prnajal Jain <pranjaljain0697@gmail.com>
2023-05-03 14:15:38 -0400

    Added search bar

--- components/Nav.js
+++ components/Nav.js
@@ -1,15 +1,25 @@
+import { BsSearch } from "react-icons/bs"
+import { RxCross1 } from "react-icons/rx"
 import Styles from "../styles/Nav.module.css";
+import { useState } from "react";
 
 export default function Nav() {
+  const [showSearchBar, setShowSearchBar] = useState(false)
+
   return (
     <nav className={Styles.nav}>
       <div className={Styles.left}>MyWebClass</div>
       <div className={Styles.right}>
         <ul>
-            <li>Home</li>
-            <li>Blogs</li>
-            <li>About</li>
-            <li>Contact</li>
+          {showSearchBar ? <li>
+            <input placeholder="Search..." type="text" className={Styles.searchBar} />
+          </li> :
+            <>
+              <li>Home</li>
+              <li>Blogs</li>
+              <li>About</li>
+              <li>Contact</li></>}
+          <li onClick={() => { setShowSearchBar(!showSearchBar) }}>{!showSearchBar ? <BsSearch /> : <RxCross1 />}</li>
         </ul>
       </div>
     </nav>
